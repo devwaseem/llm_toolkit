a627ec164b4db2cc1dcef3438aee63a4fe0f4eee
Waseem Akram <waseem07799@gmail.com>
2024-09-19 20:11:01 +0530

    feat: add llm schema generator

---
+++ schema_generator/definition_driven.py
@@ -0,0 +1,167 @@
+from functools import cache
+from pathlib import Path
+from typing import Any, override
+
+from llm_toolkit.schema_generator.exceptions import (
+    InvalidSchemaError,
+)
+from llm_toolkit.schema_generator.models import (
+    Field,
+    ListField,
+    LLMSchemaGenerator,
+    LLMSchemaModel,
+)
+
+
+@cache
+def _get_example() -> str:
+    example_file = Path(__file__).parent / "examples" / "definition_driven.txt"
+    return example_file.read_text()
+
+
+class DefinitionDrivenLLMSchemaGenerator(LLMSchemaGenerator):
+    def __init__(self, *, schema: LLMSchemaModel, encode: bool = True) -> None:
+        self.schema = schema
+        self.should_encode = encode
+        self.key_cache: dict[str, str] = {}
+        self.definitions: dict[str, Any] = {}
+        self._collect_schema_definitions(schema=self.schema)
+        self.fields = self._collect_schema_fields(schema=self.schema)
+
+    def build_schema(self) -> dict[str, Any]:
+        return {"definitions": self.definitions, "fields": self.fields}
+
+    def _collect_schema_definitions(self, *, schema: LLMSchemaModel) -> None:
+        for attr in dir(schema):
+            if attr.startswith("_"):
+                continue
+
+            field = getattr(schema, attr)
+            if isinstance(field, LLMSchemaModel):
+                model_name = type(field).__name__
+                if model_name not in self.definitions:
+                    definition = {
+                        "fields": self._collect_schema_fields(schema=field)
+                    }
+                    self.definitions[model_name] = definition
+                    self._collect_schema_definitions(schema=field)
+            elif isinstance(field, ListField):
+                model_name = field.cls.__name__
+                if model_name not in self.definitions:
+                    field_instance = field.cls(**field.kwargs or {})
+                    definition = {
+                        model_name: {
+                            "fields": self._collect_schema_fields(
+                                schema=field_instance
+                            ),
+                        }
+                    }
+                    self.definitions[model_name] = definition
+                    self._collect_schema_definitions(schema=field_instance)
+
+    def _collect_schema_fields(
+        self, *, schema: LLMSchemaModel
+    ) -> dict[str, Any]:
+        fields = {}
+        for attr in dir(schema):
+            if attr.startswith("_"):
+                continue
+
+            field = getattr(schema, attr)
+
+            if isinstance(field, Field):
+                schema_obj = {}
+                if self.should_encode:
+                    schema_obj["name"] = attr
+                if field.description:
+                    schema_obj["description"] = field.description
+                if field.extra_args:
+                    schema_obj.update(field.extra_args)
+
+                schema_obj["type"] = field.value_type
+
+                encoded_key = (
+                    self._generate_key(original_key=attr)
+                    if self.should_encode
+                    else attr
+                )
+                fields[encoded_key] = schema_obj
+                self.key_cache[encoded_key] = attr
+            elif isinstance(field, LLMSchemaModel):
+                model_name = type(field).__name__
+                schema_obj = {}
+                if self.should_encode:
+                    schema_obj["name"] = attr
+
+                if field.description:
+                    schema_obj["description"] = field.description
+
+                schema_obj["type"] = "object"
+                schema_obj["object_type"] = model_name
+
+                encoded_key = (
+                    self._generate_key(original_key=attr)
+                    if self.should_encode
+                    else attr
+                )
+                fields[encoded_key] = schema_obj
+                self.key_cache[encoded_key] = attr
+
+            elif isinstance(field, ListField):
+                schema_obj = {}
+                model_name = field.cls.__name__
+                field_instance = field.cls(**field.kwargs or {})
+
+                if self.should_encode:
+                    schema_obj["name"] = attr
+
+                if field_instance.description:
+                    schema_obj["description"] = field_instance.description
+
+                schema_obj["type"] = "list"
+                schema_obj["list_object"] = model_name
+
+                encoded_key = (
+                    self._generate_key(original_key=attr)
+                    if self.should_encode
+                    else attr
+                )
+                fields[encoded_key] = schema_obj
+                self.key_cache[encoded_key] = attr
+
+        return fields
+
+    @override
+    def is_encoded(self) -> bool:
+        return self.should_encode
+
+    @override
+    def decode_json(self, *, data: dict[str, Any]) -> dict[str, Any]:
+        decoded_schema: dict[str, Any] = {}
+        for key, value in data.items():
+            if key not in self.key_cache:
+                raise InvalidSchemaError(
+                    f"Key {key} not encoded using this instance"
+                )
+
+            decoded_key = self.key_cache[key]
+            if isinstance(value, dict):
+                decoded_schema[decoded_key] = self.decode_json(data=value)
+            elif isinstance(value, list):
+                decoded_schema[decoded_key] = [
+                    self.decode_json(data=item) for item in value
+                ]
+            else:
+                decoded_schema[decoded_key] = value
+        return decoded_schema
+
+    def _generate_key(self, *, original_key: str) -> str:
+        int_key = 0
+        for char in original_key:
+            int_key += ord(char)
+        return str(int_key % 99) + "".join(
+            [i[0] for i in original_key.split("_")]
+        )
+
+    def get_example(self) -> str:
+        return _get_example()
---
+++ schema_generator/models.py
@@ -0,0 +1,273 @@
+import copy
+import json
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, Generic, Type, TypeVar, override
+
+T = TypeVar("T", None, int, float, str, bool, datetime, list[Any])
+
+
+class Field(Generic[T], ABC):
+    def __init__(
+        self,
+        description: str,
+        *,
+        value_type: str,
+        extra_kwargs: dict[str, Any] | None = None,
+        default: T | None = None,
+    ) -> None:
+        self._value: T | None = None
+        self.description = description
+        self.value_type = value_type
+        self.format = format
+        self.default: T | None = default
+        self.extra_args = extra_kwargs
+
+    def set_value(self, value: T | None) -> None:
+        self._value = value
+
+    @abstractmethod
+    def get_value(self) -> T | None:
+        raise NotImplementedError
+
+
+class IntegerField(Field[int]):
+    def __init__(
+        self,
+        description: str = "",
+        *,
+        extra_kwargs: dict[str, Any] | None = None,
+        default: int | None = None,
+    ) -> None:
+        super().__init__(
+            description=description,
+            value_type="integer",
+            extra_kwargs=extra_kwargs,
+            default=default,
+        )
+
+    @override
+    def get_value(self) -> int | None:
+        return int(self._value) if self._value is not None else self.default
+
+
+class FloatField(Field[float]):
+    def __init__(
+        self,
+        description: str = "",
+        *,
+        extra_kwargs: dict[str, Any] | None = None,
+        default: float | None = None,
+    ) -> None:
+        super().__init__(
+            description=description,
+            value_type="float",
+            extra_kwargs=extra_kwargs,
+            default=default,
+        )
+
+    @override
+    def get_value(self) -> float | None:
+        return float(self._value) if self._value is not None else self.default
+
+
+class StringField(Field[str]):
+    def __init__(
+        self,
+        description: str = "",
+        *,
+        extra_kwargs: dict[str, Any] | None = None,
+        default: str | None = None,
+    ) -> None:
+        super().__init__(
+            description=description,
+            value_type="string",
+            extra_kwargs=extra_kwargs,
+            default=default,
+        )
+
+    @override
+    def get_value(self) -> str | None:
+        return str(self._value) if self._value is not None else self.default
+
+
+class DateTimeField(Field[datetime]):
+    def __init__(
+        self,
+        description: str = "",
+        *,
+        extra_kwargs: dict[str, Any] | None = None,
+        default: datetime | None = None,
+    ) -> None:
+        if extra_kwargs is None:
+            extra_kwargs = {}
+
+        extra_kwargs["format"] = "isoformat"
+        extra_kwargs["example"] = "2000-10-31T01:30:00.000Z"
+        super().__init__(
+            description=description,
+            value_type="datetime with timezone",
+            extra_kwargs=extra_kwargs,
+            default=default,
+        )
+
+    @override
+    def get_value(self) -> datetime | None:
+        return self._value if self._value is not None else self.default
+
+
+class BooleanField(Field[bool]):
+    def __init__(
+        self,
+        description: str = "",
+        *,
+        default: bool | None = None,
+    ) -> None:
+        super().__init__(
+            description=description,
+            value_type="boolean",
+            default=default,
+        )
+
+    def get_value(self) -> bool | None:
+        return self._value
+
+
+F = TypeVar("F", bound="LLMSchemaModel | Field[Any]")
+
+
+class ListField(Generic[F]):
+    cls: Type[F]
+    kwargs: dict[str, Any] | None = None
+    _value: list[F]
+
+    def __init__(
+        self,
+        cls: Type[F],
+        *,
+        kwargs: dict[str, Any] | None = None,
+    ) -> None:
+        self._value = []
+        self.cls = cls
+        self.kwargs = kwargs
+
+    def set_value(self, value: list[F]) -> None:
+        self._value = value
+
+    def get_value(self) -> list[F]:
+        return self._value
+
+
+class LLMSchemaModel:
+    schema_model_name = "Schema"
+
+    def __init__(
+        self,
+        description: str = "",
+        data: dict[str, Any] | None = None,
+    ) -> None:
+        self.description = description
+        if data:
+            self.load_data(data=data)
+
+    def load_data(self, *, data: dict[str, Any]) -> None:
+        for attr in dir(self):
+            if attr.startswith("_"):
+                continue
+
+            if attr not in data:
+                continue
+
+            value = data[attr]
+
+            if not value:
+                continue
+
+            field = copy.deepcopy(getattr(self, attr))
+            if isinstance(field, DateTimeField):
+                datetime_value: datetime | None = None
+                if value:
+                    datetime_value = datetime.fromisoformat(value)
+                field.set_value(datetime_value)
+            elif isinstance(field, Field):
+                field.set_value(value)
+            elif isinstance(field, ListField):
+                instances = [
+                    self._create_instance(
+                        cls=field.cls,
+                        kwargs=field.kwargs or {},
+                        data=item,
+                    )
+                    for item in value
+                ]
+                field.set_value(instances)
+
+            elif isinstance(field, LLMSchemaModel):
+                field.load_data(data=value)
+
+            setattr(self, attr, field)
+
+    def _create_instance(
+        self,
+        *,
+        cls: Type["LLMSchemaModel"] | Type[Field[Any]],
+        kwargs: dict[str, Any],
+        data: Any,
+    ) -> "LLMSchemaModel | Field[Any]":
+        instance = cls(**kwargs)
+        if isinstance(instance, LLMSchemaModel):
+            instance.load_data(data=data)
+            return instance
+
+        if isinstance(instance, Field):
+            instance.set_value(data)
+            return instance
+
+        raise NotImplementedError
+
+    def to_dict(self) -> dict[str, Any]:
+        value = {}
+        for attr in dir(self):
+            if attr.startswith("_"):
+                continue
+
+            attr_value = getattr(self, attr)
+
+            if isinstance(attr_value, Field):
+                value[attr] = attr_value.get_value()
+
+            elif isinstance(attr_value, LLMSchemaModel):
+                value[attr] = attr_value.to_dict()
+
+            elif isinstance(attr_value, ListField):
+                item_value_list = []
+                for item in attr_value.get_value():
+                    if isinstance(item, Field):
+                        item_value_list.append(item.get_value())
+                    elif isinstance(item, LLMSchemaModel):
+                        item_value_list.append(item.to_dict())
+
+                value[attr] = item_value_list
+
+        return value
+
+    def __repr__(self) -> str:
+        return json.dumps(self.to_dict())
+
+
+class LLMSchemaGenerator(ABC):
+    @abstractmethod
+    def build_schema(self) -> dict[str, Any]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_encoded(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def decode_json(self, *, data: dict[str, Any]) -> dict[str, Any]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_example(self) -> str:
+        raise NotImplementedError
